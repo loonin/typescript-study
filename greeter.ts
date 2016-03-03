8cd09e425169f65194e36c2dda7592ab06a69c19
Лунин Алексей <info@lynin.ru>
2016-03-03 22:56:26 +0300

    Make the interface.

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,12 @@
-﻿function greeter(person: string) {
-    return "Hello, " + person;
+﻿interface Person {
+    firstname: string;
+    lastname: string;
 }
 
-var user = "Jane User";
+function greeter(person : Person) {
+    return "Hello, " + person.firstname + " " + person.lastname;
+}
+
+var user = {firstname: "Jane", lastname: "User"};
 
 document.body.innerHTML = greeter(user);
