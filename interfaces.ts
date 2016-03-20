929605ac1213bef9018150fc8d080c08d9fe9078
Лунин Алексей <info@lynin.ru>
2016-03-20 21:58:00 +0300

    Interfaces. Beginning.

---
+++ interfaces.ts
@@ -0,0 +1,45 @@
+interface LabelledValue {
+  label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+  console.log(labelledObj.label);
+}
+
+var myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+//-----------------------
+
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+function createSquare(config: SquareConfig): {color: string; area: number} {
+  var newSquare = {color: "white", area: 100};
+  if (config.color) {
+    newSquare.color = config.color;
+  }
+  if (config.width) {
+    newSquare.area = config.width * config.width;
+  }
+  return newSquare;
+}
+
+var mySquare = createSquare({color: "black"});
+
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+var mySearch: SearchFunc;
+mySearch = function(src: string, sub: string) {
+  var result = src.search(sub);
+  if (result == -1) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
